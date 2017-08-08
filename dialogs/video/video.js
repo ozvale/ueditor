14e2c19a83aa30fe37ffbe0859dc73d20f164259
holy kitty in the universe <605231451@qq.com>
2017-08-08 15:13:54 +0800

    修复元素是否含有class edui-faked-video 的判断bug

--- dialogs/video/video.js
+++ dialogs/video/video.js
@@ -49,7 +49,7 @@
         (function(){
             var img = editor.selection.getRange().getClosedNode(),url;
             if(img && img.className){
-                var hasFakedClass = (img.className == "edui-faked-video"),
+                var hasFakedClass = img.className.indexOf("edui-faked-video")!=-1,
                     hasUploadClass = img.className.indexOf("edui-upload-video")!=-1;
                 if(hasFakedClass || hasUploadClass) {
                     $G("videoUrl").value = url = img.getAttribute("_url");
